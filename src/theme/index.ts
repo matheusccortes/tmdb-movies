2f99bcf3e22278f5885d615ea974e41e1580998e
Matheus Cortes <matheus.cortes@grupointegrado.br>
2023-09-16 15:17:55 -0300

    Create the application's theme settings

---
+++ src/theme/index.ts
@@ -0,0 +1,34 @@
+export const theme = {
+  colors: {
+    background: '#FDFDFD',
+    primary: {
+      900: '#161616',
+      700: '#4A4A4A',
+      200: '#EEEEEE',
+    },
+    secondary: {
+      900: '#3B82F6',
+      200: '#A6C3F1',
+    },
+  },
+  spacing: {
+    sm: 4,
+    md: 8,
+    lg: 12,
+    xl: 16,
+    '2xl': 32,
+    '3xl': 64,
+  },
+  fontSize: {
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 20,
+    '2xl': 24,
+    '3xl': 28,
+  },
+  borderRadius: {
+    sm: 4,
+    md: 8,
+  },
+};
