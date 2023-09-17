d7d610114118e0ccaa2b50117ac528b5a13df644
Matheus Cortes <matheus.cortes@grupointegrado.br>
2023-09-17 18:45:53 -0300

    Create domain type to represent movie details

--- src/domain/movie/types.ts
+++ src/domain/movie/types.ts
@@ -14,3 +14,27 @@ export type ListMovie = {
   vote_average: number;
   vote_count: number;
 };
+
+type Genre = {
+  id: number;
+  name: string;
+};
+
+type SpokenLanguage = {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+};
+
+type ProductionCountry = {
+  iso_3166_1: string;
+  name: string;
+};
+
+export type Movie = Omit<ListMovie, 'genre_ids'> & {
+  budget: number;
+  genres: Genre[];
+  revenue: number;
+  spoken_languages: SpokenLanguage[];
+  production_countries: ProductionCountry[];
+};
